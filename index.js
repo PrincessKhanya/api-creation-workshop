5fbbb9113695db15b8fbd4c53f861ada66dcd13d
Nokukhanya Makhanya <nokhanyamakhanya@gmail.com>
2022-04-06 21:52:09 +0200

    adding new garmets

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const garments = require('./garments.json');
 // enable the static folder...
 app.use(express.static('public'));
 
+// enable the req.body object - to allow us to use HTML forms
+// when doing post requests
+// put this before you declare any routes
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // import the dataset to be used here
 
 const PORT = process.env.PORT || 4017;
@@ -29,7 +36,7 @@ app.get('/api/garments', function(req, res){
     });
 	// note that this route just send JSON data to the browser
 	// there is no template
-	res.json({garments});
+	res.json({garments: filteredGarments});
 });
 
 app.get('/api/garments/price/:price', function(req, res){
@@ -46,6 +53,70 @@ app.get('/api/garments/price/:price', function(req, res){
 		garments : filteredGarments
 	});
 });
+
+let values = {
+	"description": "Rainbow unicorn sweater",
+	"price": 799.00,
+	"img": "placeholder.png",
+	"gender": "Unisex",
+	"season": "All season"
+}
+
+app.post('/api/garments', (req, res) => {
+
+	// get the fields send in from req.body
+	const {
+		description,
+		img,
+		gender,
+		season,
+		price
+	} = req.body;
+
+	// add some validation to see if all the fields are there.
+	// only 3 fields are made mandatory here
+	// you can change that
+
+	if (!description || !img || !price) {
+		res.json({
+			status: 'error',
+			message: 'Required data not supplied',
+		});
+	} else {
+
+		//you can check for duplicates here using garments.find
+		
+		function isAdult(member) {
+			return member.description==values.description, member.img==values.img
+		}
+		let duplicate=garments.find(isAdult);
+
+		if (duplicate == undefined) {
+			//add a new entry into the garments list
+			garments.push({
+				description,
+				img,
+				gender,
+				season,
+				price
+			});
+
+			res.json({
+				status: 'success',
+				message: 'New garment added.',
+				duplicate: duplicate,
+			});
+		} else {
+			res.json({
+				status: 'error',
+				message: 'Required data supplied exists',
+				duplicate: duplicate,
+			});	
+		}
+	}
+
+});
+
 
 app.listen(PORT, function() {
 	console.log(`App started on port ${PORT}`)
